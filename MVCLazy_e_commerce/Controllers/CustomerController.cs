892d2361170dfd57744c5d022b838b6da7b08bc3
Cgy-137 <62367109+Cgy-137@users.noreply.github.com>
2020-04-27 09:49:17 +0800

    添加项目文件。

---
+++ MVCLazy_e_commerce/Controllers/CustomerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLazy_e_commerce.Controllers
+{
+    /// <summary>
+    /// 订单控制器
+    /// </summary>
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 显示订单视图
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult xian()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCLazy_e_commerce/Controllers/ShopGoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLazy_e_commerce.Controllers
+{
+    /// <summary>
+    /// 商品购物车控制器
+    /// </summary>
+    public class ShopGoController : Controller
+    {
+        // GET: ShopGo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 购物车列表显示
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Xian()
+        {
+            return View();
+        }
+
+    }
+}
